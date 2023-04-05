9392f8820bc49d95ec9f7a046087073fb2f3f236
JeweIxx <vhiunese@gmail.com>
2023-04-05 17:23:00 +0400

    fixed middleware and added trpc route

--- src/pages/middleware.ts
+++ src/pages/middleware.ts
@@ -7,17 +7,17 @@ export default withClerkMiddleware((req: NextRequest) => {
     
   return NextResponse.next();
 });
- 
+  
 export const config = {
   matcher: [
     /*
      * Match all request paths except for the ones starting with:
      * - _next
      * - static (static files)
-     * - favicon.ico (favicon file)  
+     * - favicon.ico (favicon file)   
      * - public folder
      */
     "/((?!static|.*\\..*|_next|favicon.ico).*)",
     "/",
   ],
-}
+} 
